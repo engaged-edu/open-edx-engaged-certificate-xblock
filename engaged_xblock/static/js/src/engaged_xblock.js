692f037111d1bd89368bc3bd090f77b62a7ec483
gustavo.santos97 <gustavoms97@hotmail.com>
2020-10-05 14:50:38 -0300

    feat: add loading indicator so that the page does not stay still to the student

--- engaged_xblock/static/js/src/engaged_xblock.js
+++ engaged_xblock/static/js/src/engaged_xblock.js
@@ -15,24 +15,32 @@ function EngagEDXBlock(runtime, element) {
     function request_certificate(e) {
         e.preventDefault();
         const lead_full_name = $('#student-name').val();
+        const requestContent = document.getElementById('request-content');
         $.ajax({
             type: "POST",
             url: handlerUrl,
             data: JSON.stringify({"custom_fields": {
                 "lead_full_name": lead_full_name
             }}),
+            beforeSend: () => {
+                const loader = document.createElement('div');
+                loader.id = 'request-loader'
+                loader.classList.add('loader')
+                requestContent.appendChild(loader);
+            },
             success: updateData,
             error: (result) => {
                 const msg = result.responseJSON && result.responseJSON.error ? result.responseJSON.error : "Não foi possivel gerar seu certificado"
-                var p = document.createElement('p');
+                const p = document.createElement('p');
                 p.innerHTML = msg;
                 p.id = 'error-message-return'
                 p.classList.add('error')
-                document.getElementById('request-content').appendChild(p);
+                document.getElementById('request-loader').remove()
+                requestContent.appendChild(p);
                 setTimeout(function remove() {
                     document.getElementById('error-message-return').remove()
                 }, 2500);
-            },
+            }
         });
         return false;
     }
